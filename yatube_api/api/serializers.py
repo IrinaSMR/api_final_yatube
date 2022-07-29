899e06abe1de332a82e48ed3b7db0403153210d6
Ирина Смирнова <ismirnova83@yandex.ru>
2022-07-29 22:31:24 +0300

    29.07.22

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,22 +2,77 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group, Follow, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ('id', 'username', 'first_name', 'last_name', )
+        model = User
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    group = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='title',
+        required=False,
+    )
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True,
+        slug_field='username'
     )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    def validate(self, data):
+        follower = self.context['request'].user
+        following = data['following']
+
+        if Follow.objects.filter(user=follower, following=following).exists():
+            raise serializers.ValidationError(
+                'Вы уже подписаны на данного автора'
+            )
+
+        if follower == following:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя'
+            )
+
+        return data
+
+    class Meta:
+        fields = ('id', 'user', 'following')
+        model = Follow
